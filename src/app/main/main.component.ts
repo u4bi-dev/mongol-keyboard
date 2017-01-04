5f156f3455aa9aae8a5c32df6e6bc98a480bf07d
u4bi <u4bi@u4bi.com>
2017-01-04 12:10:40 +0900

    constr init

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  info : any;
+  style : any;
+  nodeTable : any;
+  textArray : string[];
 
-  constructor() { }
+  constructor() {
+    let text = 'asdfasdf';
+    this.textArray = text.split("");
 
-  ngOnInit() {
+    this.info = {wordLeft:0,leftValue:150,tick:0,temp:null};
+    this.style = {
+      un:{border:'0',boxShadow:'none',fontSize:'5rem'},
+      now:{border:'1px solid red',boxShadow:'0 0 50px 0 red',fontSize:'12rem'}
+    };
+
+    window.addEventListener('keydown', this.sendKey);
+    window.addEventListener('keyup', this.outKey);
   }
 
+  ngOnInit() { 
+    console.log(this.textArray);
+    console.log(this.info.leftValue);
+    console.log(this.style.un.fontSize);
+  }
+
+  sendKey() : void{
+    console.log('send');
+  }
+  
+  outKey() : void{
+    console.log('out');
+  }
 }
